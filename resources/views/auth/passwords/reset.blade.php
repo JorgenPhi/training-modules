6df59262e59d6f0ae50d693235419bf9da426df9
Jorgen Phillips <jorgenphillips@gmail.com>
2017-12-09 16:16:48 -0600

    Fix this one too

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
-            <div class="panel-heading">Reset Password</div>
+            <h5>Reset Password</h5>
 
             <div class="panel-body">
                 <form class="form-horizontal" method="POST" action="{{ route('password.request') }}">
