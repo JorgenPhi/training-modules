2795db3f001117faebef9eaee4a97d4974f30c31
Jorgen Phillips <jorgenphillips@gmail.com>
2017-12-20 13:29:51 -0600

    LongText is overkill just a bit
    no one will be storing 4GB of text

--- database/migrations/2017_12_06_014502_create_modules_table.php
+++ database/migrations/2017_12_06_014502_create_modules_table.php
@@ -16,7 +16,7 @@ class CreateModulesTable extends Migration
         Schema::create('modules', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->longText('body');
+            $table->mediumText('body');
             $table->timestamps();
         });
     }
