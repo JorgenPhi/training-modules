0139c606330eb80e1ed0decd856de5770fad4764
Jorgen Phillips <jorgenphillips@gmail.com>
2017-12-21 15:10:50 -0600

    Pretty Editor

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Unisharp\Ckeditor\ServiceProvider::class,
 
         /*
          * Package Service Providers...
--- resources/views/pages/admin/module/list.blade.php
+++ resources/views/pages/admin/module/list.blade.php
@@ -4,7 +4,7 @@
     <h5>Modules:</h5>
     @if(count($modules) >= 1)
         @foreach($modules as $module)
-        <a class="card waves-teal waves-effect" style="width: 100%;" href="{{ url('/admin/modules/edit/'.$module->id) }}">
+        <a class="card waves-teal waves-effect" style="width: 100%;" href="{{ url('/admin/modules/'.$module->id.'/edit') }}">
             <div class="card-content">
                 <i class="material-icons right hide-on-small-only">keyboard_arrow_right</i>
                 <span style="font-weight: 400; font-weight: thin; font-size: .7em; color: #bdbdbd;" class="right">{{$module->created_at}}</span>
