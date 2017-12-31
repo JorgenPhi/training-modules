3e09b658031d8eb019dd9fdf2e6cfdafe1459194
Jorgen Phillips <jorgenphillips@gmail.com>
2017-12-30 22:29:03 -0600

    Sync composer.lock

--- config/database.php
+++ config/database.php
@@ -69,10 +69,10 @@ return [
 
         'heroku' => [  
             'driver'   => 'pgsql',
-            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'host'     => @parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => @substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => @parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => @parse_url(getenv("DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
