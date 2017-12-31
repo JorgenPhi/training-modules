bf814f824b735f4ba48e9abaf78f8888c8f486ed
Jorgen Phillips <jorgenphillips@gmail.com>
2017-12-30 22:15:38 -0600

    Heroku Fixes

--- config/database.php
+++ config/database.php
@@ -67,6 +67,17 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'heroku' => [  
+            'driver'   => 'pgsql',
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
