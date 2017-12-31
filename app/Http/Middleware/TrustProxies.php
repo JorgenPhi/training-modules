df1529ef6d1eacef84bf3f2d31aa8df7d25a6a71
Jorgen Phillips <jorgenphillips@gmail.com>
2017-12-30 22:56:52 -0600

    Oops

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,43 +7,38 @@ use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {   
+        if(env('DB_CONNECTION', 'mysql') == 'heroku') {
+            // Heroku doesn't give us a list of IPs to trust? Seriously?
+            protected $proxies = '**';
+        }
+    }
+    
     /**
      * The trusted proxies for this application.
      *
      * @var array
      */
-    if(env('DB_CONNECTION', 'mysql') == 'heroku') {
-        // Heroku doesn't give us a list of IPs to trust? Seriously?
-        protected $proxies = '**';
+    protected $proxies;
 
-        /**
-         * The current proxy header mappings.
-         *
-         * @var array
-         */
-        protected $headers = [
-            Request::HEADER_FORWARDED => null, // not set on AWS or Heroku
-            Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-            Request::HEADER_X_FORWARDED_HOST => null,  // not set on AWS or Heroku
-            Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-            Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-        ];
-
-    } else {
-        protected $proxies;
-
-        /**
-         * The current proxy header mappings.
-         *
-         * @var array
-         */
-        protected $headers = [
-            Request::HEADER_FORWARDED => 'FORWARDED',
-            Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-            Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-            Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-            Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-        ];
-    }
+    /**
+     * The current proxy header mappings.
+     *
+     * @var array
+     */
+    protected $headers = [
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+    ];
 
 }
