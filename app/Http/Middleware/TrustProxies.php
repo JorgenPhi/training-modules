1087c428bcf2ff3d3a29afd80611c29caa1c1555
Jorgen Phillips <jorgenphillips@gmail.com>
2017-12-30 22:52:04 -0600

    Trust heroku frontend proxies.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,18 +12,38 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    if(env('DB_CONNECTION', 'mysql') == 'heroku') {
+        // Heroku doesn't give us a list of IPs to trust? Seriously?
+        protected $proxies = '**';
 
-    /**
-     * The current proxy header mappings.
-     *
-     * @var array
-     */
-    protected $headers = [
-        Request::HEADER_FORWARDED => 'FORWARDED',
-        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-    ];
+        /**
+         * The current proxy header mappings.
+         *
+         * @var array
+         */
+        protected $headers = [
+            Request::HEADER_FORWARDED => null, // not set on AWS or Heroku
+            Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+            Request::HEADER_X_FORWARDED_HOST => null,  // not set on AWS or Heroku
+            Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+            Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        ];
+
+    } else {
+        protected $proxies;
+
+        /**
+         * The current proxy header mappings.
+         *
+         * @var array
+         */
+        protected $headers = [
+            Request::HEADER_FORWARDED => 'FORWARDED',
+            Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+            Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+            Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+            Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        ];
+    }
+
 }
